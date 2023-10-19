d3f5434feeb4b6ac992c58e205716baa3161d9d6
munho ryu <munhoryu@gmail.com>
2023-10-19 20:17:57 +0900

    checking OPTCODE in types.h

--- marlinfw-study (20230914-1637)/marlinfw-study/Marlin/src/core/macros.h
+++ marlinfw-study (20230914-1637)/marlinfw-study/Marlin/src/core/macros.h
@@ -333,7 +333,9 @@ template <class V, class N1, class N2> static constexpr void LIMIT(V& v, const N
 #define _OPTARG(A,...)       , A
 #define OPTARG(O,A,...)      TERN_(O,DEFER4(_OPTARG)(A))
 #define _OPTCODE(A)         A;
-#define OPTCODE(O,A)        TERN_(O,DEFER4(_OPTCODE)(A))
+//#define OPTCODE(O,A)        TERN_(O,DEFER4(_OPTCODE)(A))
+#define OPCODE2(A) TERM_(O,DEFER4(A))
+#define OPTCODE(O,A)        OPTCODE2(O,_OPTCODE(A))
 
 // Macros to avoid 'f + 0.0' which is not always optimized away. Minus included for symmetry.
 // Compiler flags -fno-signed-zeros -ffinite-math-only also cover 'f * 1.0', 'f - f', etc.
